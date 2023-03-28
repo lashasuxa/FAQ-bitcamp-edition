76660685eef915530d2d6a76410d689a8c63778a
lashasuxa <lashasukhashvili1995@gmail.com>
2023-03-28 17:12:43 -0400

    toggle functionality JS,not closing another FAQs

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-const api_url = "/data.json";
+const api_url = "data.json";
+let FAQsElement = document.getElementById("faqs");
+
+// standard fetch starts here
+
 // fetch(api_url)
 //   .then((response) => response.json())
-//   .then((data) => console.log(data));
+//   .then((faqs) => console.log(faqs));
+
+//ends here
+
+//this is the same code for more clear picture
+
+// let getResponse = (response) => {
+//   return response.json();
+// };
+// let getJsonFromResponse = (data) => {
+
+//   console.log(data);
+// };
+// fetch(api_url).then(getResponse).then(getJsonFromResponse);
+
+// clear picture ends here
 
 // let FAQsElement = document.getElementById("faqs");
-// console.log(FAQsElement);
 
-
-
-
-let getResponse=(response)=>{
-  return response.json();
-}
-let getJsonFromResponse=(faqs)=>{
-  FontFace(const faq of faqs){
-    createFaqElement(faq)
+let createFaqElement = (faq) => {
+  const faqElement = document.createElement("li");
+  faqElement.textContent = faq.name;
+  const faqElementDescription = document.createElement("p");
+  faqElementDescription.textContent = faq.description;
+  faqElement.appendChild(faqElementDescription);
+  FAQsElement.appendChild(faqElement);
+  const paragraphElement = faqElement.querySelector("p");
+  if (faq.collapsed) {
+    //this if statement adds hidden to the data.json data, we can handle number or queuing of open FAQs by changing data collapsed values
+    paragraphElement.classList.add("hidden");
   }
-}
+  faqElement.addEventListener("click", (event) => {
+    paragraphElement.classList.toggle("hidden");
+  });
+};
 
 fetch(api_url)
-.then(getResponse)
-.then(getJsonFromResponse)
+  .then((response) => response.json())
+  .then((faqs) => {
+    for (const faq of faqs) {
+      createFaqElement(faq);
+    }
+  });
